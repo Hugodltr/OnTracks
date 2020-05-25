ab629f0b963284a517148d29f404ae96cfa3e588
55750061 <delattre.hugo@gmail.com>
2020-05-25 21:09:49 +0200

    Initial commit

---
+++ app/src/main/java/com/epf/ontracks/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.epf.ontracks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
